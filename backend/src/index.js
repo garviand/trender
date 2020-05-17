27ae744a75eb3309099be105b017ee46318a2d08
Andy Garvin <garvin.andy@gmail.com>
2020-05-17 15:12:43 -0500

    add express typescript backend and include that in docker

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = express_1["default"]();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log('App is listening on port 3000!');
+});
